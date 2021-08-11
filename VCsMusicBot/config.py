f66b448ae40322675b6994b743e8db65ef2a8f9e
conversatie <florea.ionut@outlook.com>
2021-08-11 04:45:22 +0300

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -8,7 +8,7 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "BAC1roB8z8C3Tjaj5HKohpJvVl0q7COw_5fYS4Df5zSsCzymEO-E7XHUDn7V7aDxqkyTsyAzoYFmLSqpteERvvMRMw7ItcLb9AzeXTyS6ZgdzS4hm1rHoyWQlmUdCGTd4oo5x5pArk50xymUEuflZ9h7PFidptMsfC4Sp6b8yLqTlILAEN1ZEzWStp3U0XHyzkcAUbCTyGjB5s6RV1uQdvQvNxWMj-w5dbq9x-qfF2O3czAVICJo2gGd29KvNaJ6kzX46nbayw4l5HIlEj0Aiply5PvShcysFzVVO9MdPSEzAyHyktdvCMAZkK2RYLATlB9hSuUQEZmKKdHNfjBcp7OqaFaYTQA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1842527431:AAHzfatT8Lpa-t00tghEHnpPzpzCwL0xDNI")
+BOT_TOKEN = getenv("BOT_TOKEN", "1920790228:AAF0PFC4_HrbaMuPgUWrFD20GEpi0xwPsjg")
 BOT_NAME = getenv("BOT_NAME", "Durex")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
